eedde4f32d7b049f5dc5d1af1b1fb11852448f50
xer <xer@srclo.net>
2026-02-12 13:24:37 -0500

    Update config.mk

--- config.mk
+++ config.mk
@@ -1,39 +1,39 @@
+
+# compiler and linker
+CC = cc
 # dwm version
 VERSION = 6.6
 
 # Customize below to fit your system
 
 # paths
-PREFIX = /usr/local
+# Guix doesn't allow writing to /usr/local. Install to your home folder instead.
+PREFIX = ${HOME}/.local
 MANPREFIX = ${PREFIX}/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+# Use pkg-config to find X11 paths
+X11INC = $(shell pkg-config --cflags x11)
+X11LIB = $(shell pkg-config --libs x11)
 
 # Xinerama, comment if you don't want it
-XINERAMALIBS  = -lXinerama
+XINERAMALIBS  = $(shell pkg-config --libs xinerama)
 XINERAMAFLAGS = -DXINERAMA
 
-# freetype
-FREETYPELIBS = -lfontconfig -lXft
-FREETYPEINC = /usr/include/freetype2
-# OpenBSD (uncomment)
-#FREETYPEINC = ${X11INC}/freetype2
-#MANPREFIX = ${PREFIX}/man
+# freetype / Xft
+# This fixes the ft2build.h and Xft.h errors by pulling correct store paths
+FREETYPELIBS = $(shell pkg-config --libs xft)
+FREETYPEINC = $(shell pkg-config --cflags xft)
 
 # includes and libs
-INCS = -I${X11INC} -I${FREETYPEINC}
-LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}
+# Note: FREETYPEINC already includes the -I prefix
+INCS = ${X11INC} ${FREETYPEINC}
+LIBS = ${X11LIB} ${XINERAMALIBS} ${FREETYPELIBS}
 
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
-#CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
 CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
 LDFLAGS  = ${LIBS}
 
-# Solaris
-#CFLAGS = -fast ${INCS} -DVERSION=\"${VERSION}\"
-#LDFLAGS = ${LIBS}
-
 # compiler and linker
-CC = cc
+
+CC = gcc
